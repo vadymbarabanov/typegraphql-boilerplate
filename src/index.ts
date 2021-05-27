365b8bcb890e380c1ee170493f1f5f0e5a8fc891
Vadim Barabanov <introvert.png@gmail.com>
2021-05-27 15:58:34 +0300

    Add dotenv config

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
 import "reflect-metadata"
+import "dotenv/config"
 import { ApolloServer } from "apollo-server-express"
-import { buildSchema, Resolver, Query } from "type-graphql"
+import { buildSchema } from "type-graphql"
 import express from "express"
 import { createConnection } from "typeorm"
 
-const PORT = process.env.PORT || 4000
+import { RegisterResolver } from "./resolvers/user/Register"
 
-@Resolver()
-class User {
-  @Query(() => String)
-  getUser() {
-    return "user"
-  }
-}
+const PORT = process.env.PORT || 5000
 
 const main = async () => {
   await createConnection()
+
+  // Creating GraphQL schema
   const schema = await buildSchema({
-    resolvers: [User],
+    resolvers: [RegisterResolver],
   })
+
   const apolloServer = new ApolloServer({ schema })
+
   const app = express()
   apolloServer.applyMiddleware({ app })
+
   app.listen(PORT, () => {
     console.log(`💫 Server started on http://localhost:${PORT}/graphql`)
   })
